b93bd69842ef4a01cf11921246a0e7e1d9797551
ediazjz <iam.ediazjz@gmail.com>
2024-03-15 10:01:21 -0600

    build: upgrade to Storybook 8

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -8,7 +8,6 @@ import "../app/globals.css"
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
