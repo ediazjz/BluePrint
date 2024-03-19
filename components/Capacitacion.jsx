f8ae6df7cb432356cb98d0bd42258a04c93ac93b
ediazjz <iam.ediazjz@gmail.com>
2024-03-19 08:40:55 -0600

    merge to push

--- components/Capacitacion.jsx
+++ components/Capacitacion.jsx
@@ -41,114 +41,114 @@ export const Capacitacion = () => {
       </RadioGroup.Label>
 
       <div className="group mt-4 grid grid-cols-1 gap-y-6 odd:bg-blue-400 sm:grid-cols-3 sm:gap-x-4">
-          <RadioGroup.Option
-            key={mailingList.id}
-            value={mailingList}
-            className={({ active }) =>
-              classNames(
-                active
-                  ? "border-indigo-600 ring-2 ring-indigo-600"
-                  : "border-gray-300",
-                "relative flex cursor-pointer rounded-lg border p-4 shadow-sm transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 group-hover:bg-blue-800"
-              )
-            }
-          >
-            {({ checked, active }) => (
-              <>
-                <span className="flex flex-1">
-                  <span className="flex flex-col">
-                    <RadioGroup.Label
-                      as="span"
-                      className="block text-sm font-medium text-gray-900"
-                    >
-                      {mailingList.title}
-                    </RadioGroup.Label>
-                    <RadioGroup.Description
-                      as="span"
-                      className="mt-1 flex items-center text-sm text-gray-500"
-                    >
-                      {mailingList.description}
-                    </RadioGroup.Description>
-                    <RadioGroup.Description
-                      as="span"
-                      className="mt-6 text-sm font-medium text-gray-900"
-                    >
-                      {mailingList.users}
-                    </RadioGroup.Description>
-                  </span>
+        <RadioGroup.Option
+          key={mailingList.id}
+          value={mailingList}
+          className={({ active }) =>
+            classNames(
+              active
+                ? "border-indigo-600 ring-2 ring-indigo-600"
+                : "border-gray-300",
+              "relative flex cursor-pointer rounded-lg border p-4 shadow-sm transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 group-hover:bg-blue-800"
+            )
+          }
+        >
+          {({ checked, active }) => (
+            <>
+              <span className="flex flex-1">
+                <span className="flex flex-col">
+                  <RadioGroup.Label
+                    as="span"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    {mailingList.title}
+                  </RadioGroup.Label>
+                  <RadioGroup.Description
+                    as="span"
+                    className="mt-1 flex items-center text-sm text-gray-500"
+                  >
+                    {mailingList.description}
+                  </RadioGroup.Description>
+                  <RadioGroup.Description
+                    as="span"
+                    className="mt-6 text-sm font-medium text-gray-900"
+                  >
+                    {mailingList.users}
+                  </RadioGroup.Description>
                 </span>
-                <CheckCircleIcon
-                  className={classNames(
-                    !checked ? "invisible" : "",
-                    "h-5 w-5 text-indigo-600"
-                  )}
-                  aria-hidden="true"
-                />
-                <span
-                  className={classNames(
-                    active ? "border" : "border-2",
-                    checked ? "border-indigo-600" : "border-transparent",
-                    "-inset-px pointer-events-none absolute rounded-lg"
-                  )}
-                  aria-hidden="true"
-                />
-              </>
-            )}
-          </RadioGroup.Option>
-          <RadioGroup.Option
-            key={mailingList.id}
-            value={mailingList}
-            className={({ active }) =>
-              classNames(
-                active
-                  ? "border-indigo-600 ring-2 ring-indigo-600"
-                  : "border-gray-300",
-                "relative flex cursor-pointer rounded-lg border p-4 shadow-sm transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 group-hover:bg-blue-800"
-              )
-            }
-          >
-            {({ checked, active }) => (
-              <>
-                <span className="flex flex-1">
-                  <span className="flex flex-col">
-                    <RadioGroup.Label
-                      as="span"
-                      className="block text-sm font-medium text-gray-900"
-                    >
-                      {mailingList.title}
-                    </RadioGroup.Label>
-                    <RadioGroup.Description
-                      as="span"
-                      className="mt-1 flex items-center text-sm text-gray-500"
-                    >
-                      {mailingList.description}
-                    </RadioGroup.Description>
-                    <RadioGroup.Description
-                      as="span"
-                      className="mt-6 text-sm font-medium text-gray-900"
-                    >
-                      {mailingList.users}
-                    </RadioGroup.Description>
-                  </span>
+              </span>
+              <CheckCircleIcon
+                className={classNames(
+                  !checked ? "invisible" : "",
+                  "h-5 w-5 text-indigo-600"
+                )}
+                aria-hidden="true"
+              />
+              <span
+                className={classNames(
+                  active ? "border" : "border-2",
+                  checked ? "border-indigo-600" : "border-transparent",
+                  "-inset-px pointer-events-none absolute rounded-lg"
+                )}
+                aria-hidden="true"
+              />
+            </>
+          )}
+        </RadioGroup.Option>
+        <RadioGroup.Option
+          key={mailingList.id}
+          value={mailingList}
+          className={({ active }) =>
+            classNames(
+              active
+                ? "border-indigo-600 ring-2 ring-indigo-600"
+                : "border-gray-300",
+              "relative flex cursor-pointer rounded-lg border p-4 shadow-sm transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 group-hover:bg-blue-800"
+            )
+          }
+        >
+          {({ checked, active }) => (
+            <>
+              <span className="flex flex-1">
+                <span className="flex flex-col">
+                  <RadioGroup.Label
+                    as="span"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    {mailingList.title}
+                  </RadioGroup.Label>
+                  <RadioGroup.Description
+                    as="span"
+                    className="mt-1 flex items-center text-sm text-gray-500"
+                  >
+                    {mailingList.description}
+                  </RadioGroup.Description>
+                  <RadioGroup.Description
+                    as="span"
+                    className="mt-6 text-sm font-medium text-gray-900"
+                  >
+                    {mailingList.users}
+                  </RadioGroup.Description>
                 </span>
-                <CheckCircleIcon
-                  className={classNames(
-                    !checked ? "invisible" : "",
-                    "h-5 w-5 text-indigo-600"
-                  )}
-                  aria-hidden="true"
-                />
-                <span
-                  className={classNames(
-                    active ? "border" : "border-2",
-                    checked ? "border-indigo-600" : "border-transparent",
-                    "-inset-px pointer-events-none absolute rounded-lg"
-                  )}
-                  aria-hidden="true"
-                />
-              </>
-            )}
-          </RadioGroup.Option>
+              </span>
+              <CheckCircleIcon
+                className={classNames(
+                  !checked ? "invisible" : "",
+                  "h-5 w-5 text-indigo-600"
+                )}
+                aria-hidden="true"
+              />
+              <span
+                className={classNames(
+                  active ? "border" : "border-2",
+                  checked ? "border-indigo-600" : "border-transparent",
+                  "-inset-px pointer-events-none absolute rounded-lg"
+                )}
+                aria-hidden="true"
+              />
+            </>
+          )}
+        </RadioGroup.Option>
       </div>
     </RadioGroup>
   )
